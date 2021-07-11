0a38fc1ea853442ecb6a8ffc41af56264e458b44
EailFly Bai <eailfly@gmail.com>
2021-07-11 20:42:24 +0800

    Finish share by email

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
